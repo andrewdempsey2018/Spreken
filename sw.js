fa2403b4d83105b992da914badbfce35ede17eb6
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2026-09-21 21:39:44 +0100

    remove app

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Minimal service worker required for PWA installation
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return new Response("You are offline.");
-    })
-  );
-});
